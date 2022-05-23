66e5eb66c744ec664a03dd0534c309d3f08822d6
Tofunmi1 <folakemiadediran14@gmail.com>
2022-05-23 04:38:17 +0100

    using openzep ERC20VotesComp stead of hardcoding

--- contract/src/mechanisms/GovernanceERC20.sol
+++ contract/src/mechanisms/GovernanceERC20.sol
@@ -1,101 +1,12 @@
 //// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 //one token equals one vote
-import "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
 
-contract GovernanceERC20 is ERC20 {
-    uint256 max_supply = 20_000_000_000_000_000_000;
+contract GovernanceERC20 is ERC20VotesComp {
+    uint256 max_supply = 10000000e18; //10,000,000 DAT
 
-    constructor() ERC20("DAO-Token", "DAT") {
+    constructor() ERC20("DAO-Token", "DAT") ERC20Permit("ERC-20 Example") {
         _mint(msg.sender, max_supply);
     }
-
-    /// @notice A checkpoint for marking number of votes from a given block
-    struct Checkpoint {
-        uint32 fromBlock;
-        uint96 votes;
-    }
-    /// @notice A record of votes checkpoints for each account, by index
-    mapping(address => mapping(uint32 => Checkpoint)) public checkpoints;
-    /// @notice The number of checkpoints for each account
-    mapping(address => uint32) public numCheckpoints;
-
-    event DelegateVotesChanged(address indexed delegate, uint previousBalance, uint newBalance);
-
-    function _transferTokens(address src,uint256 dst, uint96) internal{
-        require(src != address(0), "cannot transfer from the zero address");
-        require(dst != address(0), "cannot transfer to the zero address");
-
-        balances[src] = sub96(balances[src], amount, "transfer amount exceeds balance");
-        balances[dst] = add96(balances[dst], amount, "transfer amount overflows");
-        emit Transfer(src, dst, amount);
-
-        _moveDelegates(delegates[src], delegates[dst], amount) 
-    }
-    /**The remaining part is gotten from compound's getPriorVotes(account, blocKNumber) and compound's way of counting votes; */
-    function getPriorVotes(address account, uint256 blocKNumber)
-        public
-        view
-        returns (uint96)
-    {
-        require(
-            block.number <= blocKNumber,
-            "getPrioirVotes note yet determined"
-        );
-    }
-     function _writeCheckpoint(address delegatee, uint32 nCheckpoints, uint96 oldVotes, uint96 newVotes) internal {
-      uint32 blockNumber = safe32(block.number, "block number exceeds 32 bits");
-
-      if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
-          checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
-      } else {
-          checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
-          numCheckpoints[delegatee] = nCheckpoints + 1;
-      }
-
-      emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
-    }
-
-    function _moveDelegates(address srcRep, address dstRep, uint96 amount) internal {
-        if (srcRep != dstRep && amount > 0) {
-            if (srcRep != address(0)) {
-                uint32 srcRepNum = numCheckpoints[srcRep];
-                uint96 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
-                uint96 srcRepNew = sub96(srcRepOld, amount, "moveVotes: vote amount underflows");
-                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
-            }
-
-            if (dstRep != address(0)) {
-                uint32 dstRepNum = numCheckpoints[dstRep];
-                uint96 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
-                uint96 dstRepNew = add96(dstRepOld, amount, "vote amount overflows");
-                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
-            }
-        }
-    }
-    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
-        require(n < 2**32, errorMessage);
-        return uint32(n);
-    
-    function safe96(uint n, string memory errorMessage) internal pure returns (uint96) {
-        require(n < 2**96, errorMessage);
-        return uint96(n);
-    }
-
-    function add96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
-        uint96 c = a + b;
-        require(c >= a, errorMessage);
-        return c;
-    }
-
-    function sub96(uint96 a, uint96 b, string memory errorMessage) internal pure returns (uint96) {
-        require(b <= a, errorMessage);
-        return a - b;
-    }
-
-    function getChainId() internal pure returns (uint) {
-        uint256 chainId;
-        assembly { chainId := chainid() }
-        return chainId;
-
 }
